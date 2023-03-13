b316e864d6a9ef865e1aa02cac4c1aa8538dcfd6
Zhdanov-Vyacheslav <alex.city.jdanov@gmail.com>
2023-03-13 14:46:42 +0300

    Lesson_8 refactoring client

--- gb_chat/client/__init__.py
+++ gb_chat/client/__init__.py
@@ -1,8 +1,16 @@
 import json
+import sys
 import time
+import traceback
+from json import JSONDecodeError
 from socket import SOCK_STREAM, AF_INET, socket
+from typing import Optional
+
+from jsonschema.exceptions import ValidationError
 
 from .logger import logger
+from gb_chat.tools.validator import Validator
+from gb_chat.tools.requests import request_msg, request_presence, request_quit
 
 
 class ChatClient:
@@ -12,31 +20,11 @@ class ChatClient:
         self.socket = socket(AF_INET, SOCK_STREAM)
         self.account = config["account"]
         self.mode = mode  # Временно, для выполнения ДЗ-7
+        self.validator = Validator(config["schema"])
 
-    def presence(self) -> bytes:
-        data = {
-            "action": "presence",
-            "time": time.time(),
-            "type": "status",
-            "user": {
-                "account_name": self.account,
-                "status": "Yep, I am here!"
-            }
-        }
+    def send_data(self, *, data: dict):
         data = json.dumps(data).encode(self.encoding)
-        return data
-
-    def msg(self, to: str, msg: str) -> bytes:
-        data = {
-            "action": "msg",
-            "time": time.time(),
-            "to": to,
-            "from": self.account,
-            "encoding": self.encoding,
-            "message": msg
-        }
-        data = json.dumps(data).encode(self.encoding)
-        return data
+        self.socket.send(data)
 
     def get_data(self) -> dict:
         data = self.socket.recv(1024)
@@ -56,44 +44,92 @@ class ChatClient:
 
     def connect(self):
         logger.debug("client: {name}, try connect to {ip}:{port}".format(
-            name=self.account, ip=self._config["address"], port=self._config["port"]
+            name=self.account["login"], ip=self._config["address"], port=self._config["port"]
         ))
         self.socket.connect((self._config["address"], self._config["port"]))
         logger.info("client: {name}, connected to {ip}:{port}".format(
-            name=self.account, ip=self._config["address"], port=self._config["port"]
+            name=self.account["login"], ip=self._config["address"], port=self._config["port"]
         ))
-        presence = self.presence()
+        presence = request_presence(self.account["login"])
         logger.debug("client: {name}, try send presence: {presence}".format(
-            name=self.account, presence=presence
+            name=self.account["login"], presence=presence
         ))
-        self.socket.send(presence)
+        self.send_data(data=presence)
         data = self.get_data()
         if self.check_data(data):
             self.chat()
+
+    def action(self, data: dict) -> Optional[dict]:
+        msg = None
+        action = data["action"]
+        if action == "msg":
+            if self.validator.validate_data(action, data):
+                msg = data
+        elif action == "probe":
+            if self.validator.validate_data(action, data):
+                self.send_data(data=request_presence(self.account["login"]))
+        return msg
+
+    def receiver(self):
+        while True:
+            try:
+                data = self.get_data()
+                logger.debug("Received: {}".format(data))
+                if self.validator.validate_data("action", data):
+                    data = self.action(data)
+                    if data is not None:
+                        print("\n{sender}: {msg}".format(sender=data["from"], msg=data["message"]))
+            except (JSONDecodeError, ValidationError) as e:
+                logger.error(str(e))
+            except (OSError, ConnectionError, ConnectionAbortedError, ConnectionResetError) as ex:
+                print("Соединение с сервером, разорвано")
+                logger.critical(ex.with_traceback(traceback.print_exc()), exc_info=True)
+                break
+
+    def cli(self):
+        while True:
+            command = input("Введите команду: ")
+            if command == "help":
+                print("Доступные команды:")
+                print("name - сменить имя (до подключения)")
+                print("connect - подключиться к чату")
+                print("! - выход из cli")
+                print("exit - выход из программы")
+                print("help - доступные команды")
+            elif command == "connect":
+                print("Введите знак '!' чтобы перейти в cli ")
+                self.connect()
+            elif command == "name":
+                if sys._getframe(1).f_code.co_name == "run":
+                    name = input("Ведите имя: ")
+                    self.account["login"] = name
+                else:
+                    print("Смена имени доступна только при запуске!")
+            elif command == "!":
+                break
+            elif command == "exit":
+                try:
+                    self.send_data(data=request_quit())
+                    time.sleep(1)
+                    sys.exit(1)
+                except OSError:
+                    pass
 
     def chat(self):
         # Временная модификация, для выполнения ДЗ-7
         if self.mode == "w":
             while True:
+                addressee = input("Введите адресата: ")
                 msg = input("Сообщение: ")
-                msg = self.msg("server", msg)
+                if msg == "!" or addressee == "!":
+                    self.cli()
+                msg = request_msg(sender=self.account["login"], to=addressee, encoding=self.encoding, message=msg)
                 logger.debug("client: {name}, try send msg: {msg}".format(
-                    name=self.account, msg=msg
+                    name=self.account["login"], msg=msg
                 ))
-                self.socket.send(msg)
-                # data = self.get_data()
-                # if not self.check_data(data):
-                #     break
+                self.send_data(data=msg)
         if self.mode == "r":
-            while True:
-                data = self.get_data()
-                if data is not None:
-                    logger.debug("Server, send msg: {msg}".format(
-                        name=self.account, msg=data
-                    ))
-                    print("{user}: {msg}".format(user=data["from"], msg=data["message"]))
+            self.receiver()
 
-
-
-
-
+    def run(self):
+        self.cli()
